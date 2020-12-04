ceebdaa1ed05016b889cdc4c7c9c56b54f0fe5fe
Roy <royvou@hotmailcom>
2020-12-04 23:30:22 +0100

    Day 4 - Part 1

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_04 : BaseDay
+    {
+        private string _input;
+        private string[] _requiredKeys;
+
+        public Day_04()
+        {
+            _input = File.ReadAllText(InputFilePath);
+            _requiredKeys = new[]
+            {
+
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid",
+                //"cid" //  Missing CID is fine :)
+            };
+        }
+
+        public override string Solve_1()
+        {
+            return _input.Split(Environment.NewLine + Environment.NewLine).Select(x => x.Replace(Environment.NewLine, String.Empty)).Where(x =>
+            {
+               return _requiredKeys.All(key => x.Contains(key + ":"));
+            }).Count().ToString();
+        }
+
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
