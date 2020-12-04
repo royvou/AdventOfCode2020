4457b9505985b49911d84cd3f0ad3d0c825b27bb
Roy <royvou@hotmailcom>
2020-12-05 00:31:40 +0100

    Day 4 - Part 2

--- AdventOfCode/Day_04.cs
+++ AdventOfCode/Day_04.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using AoCHelper;
 
 namespace AdventOfCode
@@ -8,12 +9,16 @@ namespace AdventOfCode
     public class Day_04 : BaseDay
     {
         private string _input;
-        private string[] _requiredKeys;
 
         public Day_04()
         {
             _input = File.ReadAllText(InputFilePath);
-            _requiredKeys = new[]
+           
+        }
+
+        public override string Solve_1()
+        {
+           var  _requiredKeys = new[]
             {
 
                 "byr",
@@ -25,20 +30,67 @@ namespace AdventOfCode
                 "pid",
                 //"cid" //  Missing CID is fine :)
             };
-        }
-
-        public override string Solve_1()
-        {
             return _input.Split(Environment.NewLine + Environment.NewLine).Select(x => x.Replace(Environment.NewLine, String.Empty)).Where(x =>
             {
                return _requiredKeys.All(key => x.Contains(key + ":"));
             }).Count().ToString();
         }
-
-
+        
         public override string Solve_2()
         {
-            throw new NotImplementedException();
+            var  _requiredKeys = new[]
+            {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid",
+                //"cid" //  Missing CID is fine :)
+            };
+            return _input.Split(Environment.NewLine + Environment.NewLine).Select(x => x.Replace(Environment.NewLine, " ")).Where(x =>
+            {
+                return _requiredKeys.All(key => x.Contains(key + ":"));
+            }).Where(x =>
+            {
+                var allFields = x.SplitSpace();
+
+                return allFields.All(currentField =>
+                {
+                    var currentFieldKey = currentField.Substring(0, 3);
+                    var currentFieldKeyValue = currentField.Substring(4);
+
+                    var success = currentFieldKey switch
+                    {
+                        "byr" => int.Parse(currentFieldKeyValue) >= 1920 && int.Parse(currentFieldKeyValue) <= 2002,
+                        "iyr" => int.Parse(currentFieldKeyValue) >= 2010 && int.Parse(currentFieldKeyValue) <= 2020,
+                        "eyr" => int.Parse(currentFieldKeyValue) >= 2020 && int.Parse(currentFieldKeyValue) <= 2030,
+                        /*"hgt" => fieldValue[^2] switch
+                        {
+                            "cm" => int.Parse(fieldValue) >= 150 && int.Parse(fieldValue) <= 193,
+                            "in" => int.Parse(fieldValue.Substring(0, fieldValue.Length -2)) >= 59 && int.Parse(regexMatch.Groups[2].Value) <= 76
+                            _ => false
+                        },*/
+                        "hgt" => currentFieldKeyValue.Substring(currentFieldKeyValue.Length - 2) switch
+                        {
+                            "in" => int.Parse(currentFieldKeyValue.Substring(0, currentFieldKeyValue.Length - 2)) >= 59 &&
+                                    int.Parse(currentFieldKeyValue.Substring(0, currentFieldKeyValue.Length - 2)) <= 76,
+                            "cm" => int.Parse(currentFieldKeyValue.Substring(0, currentFieldKeyValue.Length - 2)) >= 150 &&
+                                    int.Parse(currentFieldKeyValue.Substring(0, currentFieldKeyValue.Length - 2)) <= 193,
+                            _ => false
+                        },
+                        "hcl" => Regex.IsMatch(currentFieldKeyValue, "^(#[0-9a-f]{6})$"),
+                        "ecl" => Regex.IsMatch(currentFieldKeyValue, "^(amb|blu|brn|gry|grn|hzl|oth)$"),
+                        "pid" => Regex.IsMatch(currentFieldKeyValue, "^([0-9]{9})$"),
+                        "cid" => true,
+                        _ => false
+                    };
+
+                    return success;
+                });
+
+            }).Count().ToString();
         }
     }
 }
