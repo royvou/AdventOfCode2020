7dbddda5e7b7e0523a16b34afe236af815ed4cc0
Roy <royvou@hotmailcom>
2020-12-06 18:46:08 +0100

    Add day template

---
+++ AdventOfCode/Day_00.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public abstract class Day_00 : BaseDay
+    {
+        private string _input;
+
+        public Day_00()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
