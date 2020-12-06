140e3b3c505fb2680ac99bfe99a57f97c804f6f3
Roy <royvou@hotmailcom>
2020-12-06 18:46:41 +0100

    Day 5 - Part 1 (Including test!)

---
+++ AdventOfCode/Day_05.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_05 : BaseDay
+    {
+        private string _input;
+
+        public Day_05()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            return _input.SplitNewLine().Select(x =>
+            {
+                return GetSeatId(x);
+            }).Max().ToString();
+        }
+
+        public int GetSeatId(string info)
+        {
+            var rowInfo = info.Substring(0, 7);
+            var row = GetRow(rowInfo);
+            var columnInfo = info.Substring(7);
+            var column = GetColumn(columnInfo);
+
+            return (row * 8) + column;
+        }
+        
+        public int GetColumn(string columnInfo)
+        {
+            int min = 0;
+            int max = 7;
+            foreach (var @char in columnInfo)
+            {
+                if (@char == 'L')
+                {
+                    max = ((min + max) / 2);
+                }
+                else if (@char == 'R')
+                {
+                    min = ((min + max) / 2) + 1;
+                }
+            }
+
+            return min;
+        }
+
+        public int GetRow(string rowInfo)
+        {
+            int min = 0;
+            int max = 127;
+            foreach (var @char in rowInfo)
+            {
+                if (@char == 'F')
+                {
+                    max = ((min + max) / 2);
+                    //min = min;
+                }
+                else if (@char == 'B')
+                {
+                    min = ((min + max) / 2) + 1;
+                }
+            }
+
+            return min; 
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2020.Tests/Day_05Tests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day_05Tests
+    {
+        public Day_05Tests()
+        {
+            Day = new Day_05();
+        }
+
+        public Day_05 Day { get; set; }
+
+        [Theory]
+        [InlineData("BFFFBBFRRR", 567)]
+        [InlineData("FFFBBBFRRR", 119)]
+        [InlineData("BBFFBBFRLL", 820)]
+        public void GetSeatIdTest(string input, int output)
+        {
+            Assert.Equal(Day.GetSeatId(input), output);
+        }
+        
+        [Theory]
+        [InlineData("BFFFBBFRRR", 70)]
+        [InlineData("FFFBBBFRRR", 14)]
+        [InlineData("BBFFBBFRLL", 102)]
+        public void GetRowTest(string input, int output)
+        {
+            Assert.Equal(Day.GetRow(input), output);
+        }
+        
+        [Theory]
+        [InlineData("BFFFBBFRRR", 7)]
+        [InlineData("FFFBBBFRRR", 7)]
+        [InlineData("BBFFBBFRLL", 4)]
+        public void GetColumnTest(string input, int output)
+        {
+            Assert.Equal(Day.GetColumn(input), output);
+        }
+    }
+}
