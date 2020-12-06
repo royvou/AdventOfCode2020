e3dee0b981c90ab3eb25d1552c9da8963ff9203c
Roy <royvou@hotmailcom>
2020-12-06 19:02:18 +0100

    Day 6 - Part 1

---
+++ AdventOfCode/Day_06.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_06 : BaseDay
+    {
+        private string _input;
+
+        public Day_06()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            return _input.Split(Environment.NewLine + Environment.NewLine).Select(x =>
+            {
+                return x.Replace(Environment.NewLine, "").Distinct().Count();
+            }).Sum().ToString();
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
