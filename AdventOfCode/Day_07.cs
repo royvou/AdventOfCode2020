6ff4675ac923d0ddf4c34d64cc0242e6a74e96ac
Roy <royvou@hotmailcom>
2020-12-07 23:00:16 +0100

    Day 7 - Part 1

---
+++ AdventOfCode/Day_07.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AoCHelper;
+
+namespace AdventOfCode
+{
+    public class Day_07 : BaseDay
+    {
+        private string _input;
+
+        public Day_07()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            var contain = "contain";
+            
+            var bags = _input.SplitNewLine().ToImmutableDictionary(
+                input => Regex.Match(input, @"^(.*?) bag(s|) contain .*").Groups[1].Value,
+                input => Regex.Matches(input, @"(\d (.*?) bag(s|))").Select(x => x.Groups[2].Value).ToArray()); //Regex.Matches(input, @"(\d(.*?)bag(s|))").Select    
+            
+            return FindBagsCount(bags, "shiny gold").ToString();
+        }
+
+        private int FindBagsCount(ImmutableDictionary<string, string[]> bags, string bagToFind)
+        {
+            return bags.Select(x => FindBagsCount(bags, x.Key, bagToFind)).Where(x => x).Count(x => x);
+        }
+        
+        private bool FindBagsCount(in ImmutableDictionary<string, string[]> bags,in string bag, in string bagToFind)//, HashSet<string> result)
+        {
+            var currentBagContents = bags[bag];
+            if (currentBagContents.Contains(bagToFind))
+            {
+                return true;
+            }
+
+            foreach (var innerBagContents in currentBagContents)
+            {
+                if (FindBagsCount(bags, innerBagContents, bagToFind))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
