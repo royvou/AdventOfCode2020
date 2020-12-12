c356992a2800c666b74a70e60e39cdf6054ef9ad
Roy <royvou@hotmailcom>
2020-12-12 16:34:44 +0100

    Day 12 - Part 2

--- AdventOfCode/Day_12.cs
+++ AdventOfCode/Day_12.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -22,37 +23,83 @@ namespace AdventOfCode
 
         public override string Solve_1()
         {
-            var directions = _input.SplitNewLine().Select(x => new Day12_Action(Enum.Parse<Day12_Direction>(x.Substring(0, 1), true), int.Parse(x.Substring(1))));
+            var directions = GetDirectionsFromInput();
 
-            var result = directions.Aggregate((north: 0, east: 0, dir: 0), ((accumulator, next) =>
+            var result = CalculatePosition(directions, new Day12_Position(0, 0, 0));
+            return (Math.Abs(result.north) + Math.Abs(result.east)).ToString();
+        }
+
+        private IEnumerable<Day12_Action> GetDirectionsFromInput() =>
+            _input.SplitNewLine().Select(x => new Day12_Action(Enum.Parse<Day12_Direction>(x.Substring(0, 1), true), int.Parse(x.Substring(1))));
+
+        private static Day12_Position CalculatePosition(IEnumerable<Day12_Action> directions, Day12_Position initialPosition)
+        {
+            var result = directions.Aggregate(initialPosition, ((accumulator, next) =>
             {
                 return next switch
                 {
-                    Day12_Action action when action.Direction == Day12_Direction.N || (action.Direction == Day12_Direction.F && accumulator.dir == 90)
-                        => (accumulator.north + next.Amount, accumulator.east, accumulator.dir),
-                    Day12_Action action when action.Direction == Day12_Direction.S || (action.Direction == Day12_Direction.F && accumulator.dir == 270)
-                        => (accumulator.north - next.Amount, accumulator.east, accumulator.dir),
-                    Day12_Action action when action.Direction == Day12_Direction.E || (action.Direction == Day12_Direction.F && accumulator.dir == 0)
-                        => (accumulator.north, accumulator.east + next.Amount, accumulator.dir),
-                    Day12_Action action when action.Direction == Day12_Direction.W || (action.Direction == Day12_Direction.F && accumulator.dir == 180)
-                        => (accumulator.north, accumulator.east - next.Amount, accumulator.dir),
-                    Day12_Action action when action.Direction == Day12_Direction.L
-                        => (accumulator.north, accumulator.east, (360 + accumulator.dir + next.Amount) % 360),
-                    Day12_Action action when action.Direction == Day12_Direction.R
-                        => (accumulator.north, accumulator.east, (360 + accumulator.dir - next.Amount) % 360),
-                    _ => (accumulator.north, accumulator.east, accumulator.dir)
+                    { } action when action.Direction == Day12_Direction.N || (action.Direction == Day12_Direction.F && accumulator.direcation == 90)
+                        => accumulator with { north = accumulator.north + next.Amount},
+                    { } action when action.Direction == Day12_Direction.S || (action.Direction == Day12_Direction.F && accumulator.direcation == 270)
+                        => accumulator with { north = accumulator.north - next.Amount},
+                    { } action when action.Direction == Day12_Direction.E || (action.Direction == Day12_Direction.F && accumulator.direcation == 0)
+                        => accumulator with { east = accumulator.east + next.Amount},
+                    { } action when action.Direction == Day12_Direction.W || (action.Direction == Day12_Direction.F && accumulator.direcation == 180)
+                        => accumulator with { east = accumulator.east - next.Amount},
+                    {Direction: Day12_Direction.L} action => accumulator with { direcation = (360 + accumulator.direcation + next.Amount) % 360},
+                    {Direction: Day12_Direction.R} action => accumulator with { direcation = (360 + accumulator.direcation - next.Amount) % 360},
+                    _ => accumulator
                 };
             }));
-            return (Math.Abs(result.north) + Math.Abs(result.east)).ToString();
+            return result;
         }
 
         public override string Solve_2()
         {
-            throw new NotImplementedException();
+            var directions = GetDirectionsFromInput();
+
+            var result = CalculatePosition2(directions, new Day12_Position(0, 0, 0, 1, 10));
+            return (Math.Abs(result.north) + Math.Abs(result.east)).ToString();
+        }
+
+        private static Day12_Position CalculatePosition2(IEnumerable<Day12_Action> directions, Day12_Position initialPosition)
+        {
+            var result = directions.Aggregate(initialPosition, ((accumulator, next) =>
+            {
+                return next switch
+                {
+                    {Direction: Day12_Direction.N} action => accumulator with { northWaypoint = accumulator.northWaypoint + next.Amount},
+                    {Direction: Day12_Direction.S} action => accumulator with { northWaypoint = accumulator.northWaypoint - next.Amount},
+                    {Direction: Day12_Direction.E} action => accumulator with { eastWaypoint = accumulator.eastWaypoint + next.Amount},
+                    {Direction: Day12_Direction.W} action => accumulator with { eastWaypoint = accumulator.eastWaypoint - next.Amount},
+                    {Direction: Day12_Direction.F} action
+                        => accumulator with {
+                            north = accumulator.north + ( accumulator.northWaypoint * action.Amount),
+                            east = accumulator.east + (accumulator.eastWaypoint * action.Amount),
+                            },
+                    {Direction: Day12_Direction.L} action => RotateWaypoint(accumulator, (360 + accumulator.direcation + next.Amount) % 360),
+                    {Direction: Day12_Direction.R} action => RotateWaypoint(accumulator, (360 + accumulator.direcation - next.Amount) % 360),
+                    _ => accumulator
+                };
+            }));
+            return result;
+        }
+
+        private static Day12_Position RotateWaypoint(Day12_Position accumulator, int accumulatorDirecation)
+        {
+            return accumulatorDirecation switch
+            {
+                //0 => null, We don't change direction?!
+                90 => accumulator with { northWaypoint = accumulator.eastWaypoint, eastWaypoint = -accumulator.northWaypoint},
+                180 => accumulator with { northWaypoint = -accumulator.northWaypoint, eastWaypoint = -accumulator.eastWaypoint},
+                270 => accumulator with { northWaypoint = -accumulator.eastWaypoint, eastWaypoint = accumulator.northWaypoint},
+            };
         }
     }
 
     public record Day12_Action(Day12_Direction Direction, int Amount);
+
+    public record Day12_Position(int north, int east, int direcation, int northWaypoint = 0, int eastWaypoint = 0);
 
     public enum Day12_Direction
     {
--- AdventOfCode2020.Tests/Day_12Tests.cs
+++ AdventOfCode2020.Tests/Day_12Tests.cs
@@ -8,10 +8,18 @@ namespace AdventOfCode2020.Tests
       
         [Theory]
         [InlineData("F10\nN3\nF7\nR90\nF11", "25")]
-        public void GetSeatIdTest(string input, string output)
+        public void Part1(string input, string output)
         {
             var Day12 = new Day_12(input);
             Assert.Equal(Day12.Solve_1(), output);
+        }
+        
+        [Theory]
+        [InlineData("F10\nN3\nF7\nR90\nF11", "286")]
+        public void Part2(string input, string output)
+        {
+            var Day12 = new Day_12(input);
+            Assert.Equal(Day12.Solve_2(), output);
         }
     }
 }
