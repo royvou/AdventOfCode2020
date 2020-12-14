29f232f5cb3a295190ee87db6a053a1f3688f144
Roy <royvou@hotmailcom>
2020-12-14 21:31:03 +0100

    Day 14 - part 1

---
+++ AdventOfCode/Day_14.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day_14 : BaseDay
+    {
+        public Day_14()
+        {
+        }
+
+        public Day_14(string input) : base(input)
+        {
+        }
+
+        public override string Solve_1()
+        {
+            var memory = new Dictionary<int, long>();
+            var inputLines = _input.SplitNewLine();
+
+            long orMask = 0l, andMask = 0l;
+            foreach (var line in inputLines)
+            {
+                if (line.StartsWith("mask = "))
+                {
+                    (orMask, andMask) = ParseMask(line.Substring("mask = ".Length));
+                }
+                else
+                {
+                    var matches = Regex.Match(line, @"mem\[(\d*)\] = (\d*)");
+                    var memoryLocation = int.Parse(matches.Groups[1].Value);
+                    var memoryValue = long.Parse(matches.Groups[2].Value);
+
+                    memory[memoryLocation] = (memoryValue | orMask) & andMask;
+                }
+            }
+
+            return memory.Values.Sum().ToString();
+        }
+
+        private (long orMask, long andMask) ParseMask(string mask)
+        {
+            long orMask = 0, andMask = 0;
+            foreach (var digit in mask)
+            {
+                orMask <<= 1;
+                andMask <<= 1;
+                switch (digit)
+                {
+                    case '1':
+                        orMask |= 1;
+                        andMask |= 1;
+                        break;
+                    case '0':
+                        orMask |= 1;
+                        break;
+                    default:
+                        andMask |= 1;
+                        break;
+                }
+            }
+
+            return (orMask, andMask);
+        }
+
+        public override string Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2020.Tests/Day_14Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day_14Tests
+    {
+        [Theory]
+        [InlineData("mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0", "165")]
+        public void Part1(string input, string output)
+        {
+            var day = new Day_14(input);
+            Assert.Equal(day.Solve_1(), output);
+        }
+
+        /*
+        [Theory]
+        [InlineData("7,13,x,x,59,x,31,19", "1068781")]
+        [InlineData("17,x,13,19", "3417")]
+        [InlineData("67,7,59,61", "754018")]
+        [InlineData("67,x,7,59,61", "779210")]
+        [InlineData("67,7,x,59,61", "1261476")]
+        [InlineData("1789,37,47,1889", "1202161486")]
+        public void Part2(string input, string output)
+        {
+            var day = new Day_13(input);
+            Assert.Equal(day.Solve_2(), output);
+        }*/
+    }
+}
