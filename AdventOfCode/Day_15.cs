bcba6b083af49a2e16e870557f6326f0e43abd35
Roy <royvou@hotmailcom>
2020-12-15 20:05:00 +0100

    Day 15

---
+++ AdventOfCode/Day_15.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_15 : BaseDay
+    {
+        public Day_15()
+        {
+        }
+
+        public Day_15(string input) : base(input)
+        {
+        }
+
+        public override string Solve_1()
+            => NthNumber(2020);
+
+        private string NthNumber(int number)
+            => GetEnumeratorFor(_input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).AsInt(), number + 1).Skip(number - 1).FirstOrDefault().ToString();
+
+        private IEnumerable<int> GetEnumeratorFor(IEnumerable<int> input, int maxTurns = int.MaxValue)
+        {
+            var history = new Dictionary<int, (int SecondLastIndex, int LatestIndex)>();
+
+            int turn = 1;
+            int lastNumber = 0;
+
+            foreach (var number in input)
+            {
+                history[number] = (-1,turn);
+                lastNumber = number;
+                yield return lastNumber;
+
+                turn += 1;
+            }
+
+            while (turn < maxTurns)
+            {
+                var historyOfLastSpoken = history[lastNumber];
+                int toSpeak;
+                if (historyOfLastSpoken.SecondLastIndex == -1)
+                {
+                    toSpeak = 0;
+                }
+                else
+                {
+                    toSpeak = historyOfLastSpoken.LatestIndex - historyOfLastSpoken.SecondLastIndex;
+                    
+                }
+
+                lastNumber = toSpeak;
+                if (history.TryGetValue(toSpeak, out var currentQueue))
+                {
+                    history[toSpeak] = (currentQueue.LatestIndex, turn);
+                }
+                else
+                {
+                    history[toSpeak]= (-1,turn);
+                }
+
+                yield return lastNumber;
+                turn += 1;
+            }
+        }
+
+
+        public override string Solve_2()
+            => NthNumber(30000000);
+    }
+}
---
+++ AdventOfCode2020.Tests/Day_15Tests.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day_15Tests
+    {
+        [Theory]
+        [InlineData("0,3,6", "436")]
+        [InlineData("1,3,2", "1")]
+        [InlineData("2,1,3", "10")]
+        [InlineData("1,2,3", "27")]
+        [InlineData("2,3,1", "78")]
+        [InlineData("3,2,1", "438")]
+        [InlineData("3,1,2", "1836")]
+        public void Part1(string input, string output)
+        {
+            var day = new Day_15(input);
+            Assert.Equal(day.Solve_1(), output);
+        }
+
+        [Theory]
+        [InlineData("0,3,6", "175594")]
+        [InlineData("1,3,2", "2578")]
+        [InlineData("2,1,3", "3544142")]
+        [InlineData("1,2,3", "261214")]
+        [InlineData("2,3,1", "6895259")]
+        [InlineData("3,2,1", "18")]
+        [InlineData("3,1,2", "362")]
+        public void Part2(string input, string output)
+        {
+            var day = new Day_15(input);
+            Assert.Equal(day.Solve_2(), output);
+        }
+    }
+}
