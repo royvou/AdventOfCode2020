7fe9c52f242cd665ed8f71a3dff147c447c4d79f
Roy <royvou@hotmailcom>
2020-12-19 14:21:10 +0100

    Day 18 - Part 1

---
+++ AdventOfCode/Day_18.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day_18 : BaseDay
+    {
+        public Day_18()
+        {
+        }
+
+        public Day_18(string input) : base(input)
+        {
+        }
+
+
+        public override string Solve_1()
+        {
+            var result = 0l;
+            var inputOperations = _input.Replace("(", "( ").Replace(")", " )");
+
+            return inputOperations.SplitNewLine().Select(x => ExecuteOperations(x.SplitSpace())).Sum().ToString();
+        }
+
+        private static long ExecuteOperations(IList<string> inputOperations)
+        {
+            var result = 0L;
+
+            var activeOperation = Day18Operator.Plus;
+            var previousOperation = Day18Operator.Plus;
+            var parenthesesCount = 0;
+            var tmpOps = new List<string>();
+            long? longValue = null;
+            for (var index = 0; index < inputOperations.Count; index++)
+            {
+                var operation = inputOperations[index];
+                Day18Operator tmpOp;
+                switch (operation)
+                {
+                    case "+" when parenthesesCount == 0:
+                        tmpOp = Day18Operator.Plus;
+                        activeOperation = tmpOp;
+                        break;
+                    case "*" when parenthesesCount == 0:
+                        tmpOp = Day18Operator.Mulitply;
+                        activeOperation = tmpOp;
+                        break;
+                    case { } str when long.TryParse(str, out var tmpInt):
+                        longValue = tmpInt;
+                        tmpOp = Day18Operator.Number;
+                        break;
+                    case "(":
+                        tmpOp = Day18Operator.StartParentheses;
+                        parenthesesCount += 1;
+                        break;
+                    case ")":
+                        tmpOp = Day18Operator.EndParentheses;
+                        parenthesesCount -= 1;
+                        break;
+                    default:
+                        tmpOp = Day18Operator.None;
+                        break;
+                }
+
+                if (
+                    parenthesesCount > 0
+                )
+                {
+                    tmpOps.Add(operation);
+                }
+
+                if (tmpOp == Day18Operator.EndParentheses && parenthesesCount == 0)
+                {
+                    //Gather all operations to execute;  
+                    longValue = ExecuteOperations(tmpOps.Skip(1).ToList());
+                    tmpOps.Clear();
+                }
+
+                if (longValue.HasValue && parenthesesCount == 0)
+                {
+                    result = activeOperation switch
+                    {
+                        Day18Operator.Plus => result + longValue.Value,
+                        Day18Operator.Mulitply => result * longValue.Value,
+                        _ => throw new ArgumentOutOfRangeException(),
+                    };
+                    longValue = default;
+                }
+
+                previousOperation = tmpOp;
+            }
+
+            return result;
+        }
+
+        public override string Solve_2() => throw new NotImplementedException();
+    }
+
+    public enum Day18Operator
+    {
+        None,
+        Plus,
+        Mulitply,
+        Number,
+        StartParentheses,
+        EndParentheses,
+    }
+}
---
+++ AdventOfCode2020.Tests/Day_18Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode;
+using Xunit;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day_18Tests
+    {
+        [Theory]
+        [InlineData("1 + 2 * 3 + 4 * 5 + 6", "71")]
+        [InlineData("1 + (2 * 3) + (4 * (5 + 6))", "51")]
+        [InlineData("2 * 3 + (4 * 5)", "26")]
+        [InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", "437")]
+        [InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "12240")]
+        [InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "13632")]
+        public void Part1(string input, string output)
+        {
+            var day = new Day_18(input);
+            Assert.Equal(day.Solve_1(), output);
+        }
+    }
+}
