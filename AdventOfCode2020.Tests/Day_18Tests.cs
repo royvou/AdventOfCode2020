f84070079084b6303208c5596ee4aed7ffd6630e
Roy <royvou@hotmailcom>
2020-12-20 01:07:34 +0100

    Add Day 18 - Part 2 Tests

--- AdventOfCode2020.Tests/Day_18Tests.cs
+++ AdventOfCode2020.Tests/Day_18Tests.cs
@@ -17,5 +17,18 @@ namespace AdventOfCode2020.Tests
             var day = new Day_18(input);
             Assert.Equal(day.Solve_1(), output);
         }
+        
+        [Theory]
+        [InlineData("1 + 2 * 3 + 4 * 5 + 6", "231")]
+        [InlineData("1 + (2 * 3) + (4 * (5 + 6))", "51")]
+        [InlineData("2 * 3 + (4 * 5)", "46")]
+        [InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", "1445")]
+        [InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "669060")]
+        [InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "23340")]
+        public void Part2(string input, string output)
+        {
+            var day = new Day_18(input);
+            Assert.Equal(day.Solve_2(), output);
+        }
     }
 }
