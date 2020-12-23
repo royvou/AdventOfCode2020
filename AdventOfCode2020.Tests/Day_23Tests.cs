ee158bf995336a3437cd7d2e964aa7257e640309
Roy <royvou@hotmailcom>
2020-12-23 14:42:29 +0100

    Extend tests with 1 to 10

--- AdventOfCode2020.Tests/Day_23Tests.cs
+++ AdventOfCode2020.Tests/Day_23Tests.cs
@@ -6,6 +6,15 @@ namespace AdventOfCode2020.Tests
     public class Day_23Tests
     {
         [Theory]
+        [InlineData("389125467", "54673289", 1)]
+        [InlineData("389125467", "32546789", 2)]
+        [InlineData("389125467", "34672589", 3)]
+        [InlineData("389125467", "32584679", 4)]
+        [InlineData("389125467", "36792584", 5)]
+        [InlineData("389125467", "93672584", 6)]
+        [InlineData("389125467", "92583674", 7)]
+        [InlineData("389125467", "58392674", 8)]
+        [InlineData("389125467", "83926574", 9)]
         [InlineData("389125467", "92658374", 10)]
         [InlineData("389125467", "67384529", 100)]
         public void Part1(string input, string output, int rounds)
